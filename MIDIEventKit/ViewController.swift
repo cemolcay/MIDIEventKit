3f4bc1c0dd1ab3f8c06a2f2fcb091bb236184de2
cemolcay <ccemolcay@gmail.com>
2018-03-07 15:21:10 +0300

    Initial Commit

---
+++ MIDIEventKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MIDIEventKit
+//
+//  Created by Cem Olcay on 7.03.2018.
+//  Copyright © 2018 cemolcay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
